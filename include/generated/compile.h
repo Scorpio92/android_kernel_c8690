f1c86e45c22c27bee25c08b706ef44870f33629c
QLyine <dsantos@cryptolab.net>
2013-12-13 17:45:29 +0000

    sched: Implement smarter wake-affine logic & writeback: fix occasional slow sync(1)

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 32 */
+/* This file is auto generated, version 36 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#32 SMP PREEMPT Thu Dec 12 13:18:33 WET 2013"
+#define UTS_VERSION "#36 SMP PREEMPT Fri Dec 13 17:04:37 WET 2013"
 #define LINUX_COMPILE_BY "qlyine"
 #define LINUX_COMPILE_HOST "zenwalk"
 #define LINUX_COMPILER "gcc version 4.7.4 20131111 (prerelease) (crosstool-NG hg+default-388793b84c11 - Linaro GCC 2013.11) "
