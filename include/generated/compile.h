5bbcde93a34fe3e25e60d8d052cb14986b8436c5
scorpio92 <scorpio92@mail.ru>
2014-01-11 22:23:27 +1100

    мусор

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 44 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#44 SMP PREEMPT Sun Jan 5 12:50:20 WET 2014"
-#define LINUX_COMPILE_BY "qlyine"
-#define LINUX_COMPILE_HOST "asterix"
-#define LINUX_COMPILER "gcc version 4.7.4 20131111 (prerelease) (crosstool-NG hg+default-388793b84c11 - Linaro GCC 2013.11) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.0.101-Exynos4Brothers-1.2+"
