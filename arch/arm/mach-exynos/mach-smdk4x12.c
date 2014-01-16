bc17627970df71c80989f3a48501377bd9453556
scorpio92 <scorpio92@mail.ru>
2014-01-16 18:33:24 +1100

    Contiguous Memory Allocator. part8

--- arch/arm/mach-exynos/mach-smdk4x12.c
+++ arch/arm/mach-exynos/mach-smdk4x12.c
@@ -34,8 +34,9 @@
 #include <linux/v4l2-mediabus.h>
 #include <linux/memblock.h>
 #include <linux/delay.h>
-#if defined(CONFIG_S5P_MEM_CMA)
-#include <linux/cma.h>
+#ifdef CONFIG_DMA_CMA
+#include <linux/dma-contiguous.h>
+#include <linux/exynos_mem.h>
 #endif
 #ifdef CONFIG_ANDROID_PMEM
 #include <linux/android_pmem.h>
@@ -6551,139 +6552,24 @@ static void __init smdk4x12_set_camera_flite_platdata(void)
 }
 #endif
 
-#if defined(CONFIG_S5P_MEM_CMA)
-static void __init exynos4_cma_region_reserve(
-			struct cma_region *regions_normal,
-			struct cma_region *regions_secure)
+#if defined(CONFIG_CMA)
+static unsigned long fbmem_start;
+static unsigned long fbmem_size;
+static int __init early_fbmem(char *p)
 {
-	struct cma_region *reg;
-	phys_addr_t paddr_last = 0xFFFFFFFF;
+	char *endp;
 
-	for (reg = regions_normal; reg->size != 0; reg++) {
-		phys_addr_t paddr;
+	if (!p)
+		return -EINVAL;
 
-		if (!IS_ALIGNED(reg->size, PAGE_SIZE)) {
-			pr_err("S5P/CMA: size of '%s' is NOT page-aligned\n",
-								reg->name);
-			reg->size = PAGE_ALIGN(reg->size);
-		}
+	fbmem_size = memparse(p, &endp);
+	if (*endp == '@')
+		fbmem_start = memparse(endp + 1, &endp);
 
-
-		if (reg->reserved) {
-			pr_err("S5P/CMA: '%s' alread reserved\n", reg->name);
-			continue;
-		}
-
-		if (reg->alignment) {
-			if ((reg->alignment & ~PAGE_MASK) ||
-				(reg->alignment & ~reg->alignment)) {
-				pr_err("S5P/CMA: Failed to reserve '%s': "
-						"incorrect alignment 0x%08x.\n",
-						reg->name, reg->alignment);
-				continue;
-			}
-		} else {
-			reg->alignment = PAGE_SIZE;
-		}
-
-		if (reg->start) {
-			if (!memblock_is_region_reserved(reg->start, reg->size)
-			    && (memblock_reserve(reg->start, reg->size) == 0))
-				reg->reserved = 1;
-			else
-				pr_err("S5P/CMA: Failed to reserve '%s'\n",
-								reg->name);
-
-			continue;
-		}
-
-		paddr = memblock_find_in_range(0, MEMBLOCK_ALLOC_ACCESSIBLE,
-						reg->size, reg->alignment);
-		if (paddr != MEMBLOCK_ERROR) {
-			if (memblock_reserve(paddr, reg->size)) {
-				pr_err("S5P/CMA: Failed to reserve '%s'\n",
-								reg->name);
-				continue;
-			}
-
-			reg->start = paddr;
-			reg->reserved = 1;
-		} else {
-			pr_err("S5P/CMA: No free space in memory for '%s'\n",
-								reg->name);
-		}
-
-		if (cma_early_region_register(reg)) {
-			pr_err("S5P/CMA: Failed to register '%s'\n",
-								reg->name);
-			memblock_free(reg->start, reg->size);
-		} else {
-			paddr_last = min(paddr, paddr_last);
-		}
-	}
-
-	if (regions_secure && regions_secure->size) {
-		size_t size_secure = 0;
-		size_t align_secure, size_region2, aug_size, order_region2;
-
-		for (reg = regions_secure; reg->size != 0; reg++)
-			size_secure += reg->size;
-
-		reg--;
-
-		/* Entire secure regions will be merged into 2
-		 * consecutive regions. */
-		align_secure = 1 <<
-			(get_order((size_secure + 1) / 2) + PAGE_SHIFT);
-		/* Calculation of a subregion size */
-		size_region2 = size_secure - align_secure;
-		order_region2 = get_order(size_region2) + PAGE_SHIFT;
-		if (order_region2 < 20)
-			order_region2 = 20; /* 1MB */
-		order_region2 -= 3; /* divide by 8 */
-		size_region2 = ALIGN(size_region2, 1 << order_region2);
-
-		aug_size = align_secure + size_region2 - size_secure;
-		if (aug_size > 0)
-			reg->size += aug_size;
-
-		size_secure = ALIGN(size_secure, align_secure);
-
-		if (paddr_last >= memblock.current_limit) {
-			paddr_last = memblock_find_in_range(0,
-					MEMBLOCK_ALLOC_ACCESSIBLE,
-					size_secure, reg->alignment);
-		} else {
-			paddr_last -= size_secure;
-			paddr_last = round_down(paddr_last, align_secure);
-		}
-
-		if (paddr_last) {
-			while (memblock_reserve(paddr_last, size_secure))
-				paddr_last -= align_secure;
-
-			do {
-				reg->start = paddr_last;
-				reg->reserved = 1;
-				paddr_last += reg->size;
-
-				if (cma_early_region_register(reg)) {
-					memblock_free(reg->start, reg->size);
-					pr_err("S5P/CMA: "
-					"Failed to register secure region "
-					"'%s'\n", reg->name);
-				} else {
-					size_secure -= reg->size;
-				}
-			} while (reg-- != regions_secure);
-
-			if (size_secure > 0)
-				memblock_free(paddr_last, size_secure);
-		} else {
-			pr_err("S5P/CMA: Failed to reserve secure regions\n");
-		}
-	}
+	return endp > p ? 0 : -EINVAL;
 }
+early_param("fbmem", early_fbmem);
+
 
 static void __init exynos4_reserve_mem(void)
 {
@@ -6912,9 +6798,24 @@ static void __init exynos4_reserve_mem(void)
 		"s5p-smem/fimc=fimc3;"
 		"s5p-smem/mfc-shm=mfc1,mfc-normal;";
 
-	cma_set_defaults(NULL, map);
+	int i;
 
-	exynos4_cma_region_reserve(regions, regions_secure);
+	s5p_cma_region_reserve(regions, regions_secure, 0, map);
+
+	if (!(fbmem_start && fbmem_size))
+		return;
+
+	for (i = 0; i < ARRAY_SIZE(regions); i++) {
+		if (regions[i].name && !strcmp(regions[i].name, "fimd")) {
+			memcpy(phys_to_virt(regions[i].start), phys_to_virt(fbmem_start), fbmem_size * SZ_1K);
+			printk(KERN_INFO "Bootloader sent 'fbmem' : %08X\n", (u32)fbmem_start);
+			break;
+		}
+	}
+}
+#else
+static inline void exynos4_reserve_mem(void)
+{
 }
 #endif
 
